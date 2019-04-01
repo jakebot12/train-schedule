f43601bf96e65ea3beaeab912df300c0d12616f3
Jake Gurley <jakemg12@gmail.com>
2019-04-01 16:34:01 -0700

    <COMMENT>

---
+++ assets/javascript/app.js
@@ -0,0 +1,7 @@
+//Declare my global variables
+//set up calls for database
+//set up code to find destination and train names via user input.
+//get schedule info in militarty time.
+//Write code that makes minutes the frequency.
+//Calculate when the next train will arrive, relative to the current time.
+//create code that ensures users from many different machines can view the same train times.
