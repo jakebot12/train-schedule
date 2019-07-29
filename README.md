65444b0d72296c43cbc58b85bdde63b98276c161
Jake Gurley <jakemg12@gmail.com>
2019-07-28 18:59:24 -0700

    Scrapped old project and started over. The firebase documentation is extremely confusing. I finally got my new versio to work, basing it off of previous class exercises

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # train-schedule
 This is an app that uses Firebase as a database to store input data.
-The app returns scheduling information about the trains and their schedules
+The app returns scheduling information about the trains and their schedules.
+Initial trains have been added to the app and database, to demostrsate basic output fields, however, there is also an input field for users to add trains of their own.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,85 @@
-//Declare my global variables
-//set up calls for database
-//set up code to find destination and train names via user input.
-//get schedule info in militarty time.
-//Write code that makes minutes the frequency.
-//Calculate when the next train will arrive, relative to the current time.
-//create code that ensures users from many different machines can view the same train times.
+$(document).ready(function() {
+ 
+  var config = {
+    apiKey: "AIzaSyCoKHz349aZt0eU5BgU8OZ1sxAHn7iB3gY",
+    authDomain: "train-scheduler-ce7a8.firebaseapp.com",
+    databaseURL: "https://train-scheduler-ce7a8.firebaseio.com",
+    projectId: "train-scheduler-ce7a8",
+    storageBucket: "train-scheduler-ce7a8.appspot.com",
+    messagingSenderId: "946454989885",
+    appId: "1:946454989885:web:d9ba0a2507c90a42"
+  };
+  firebase.initializeApp(config);
+
+  var db = firebase.database();
+
+  $("#add-train").on("click", function(event) {
+    event.preventDefault();
+
+    name = $("#name-input")
+      .val()
+      .trim();
+    destination = $("#destination-input")
+      .val()
+      .trim();
+    frequency = $("#frequency-input")
+      .val()
+      .trim();
+    first = $("#first-input")
+      .val()
+      .trim();
+
+    db.ref().push({
+      name: name,
+      destination: destination,
+      frequency: frequency,
+      first: first,
+      dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
+  });
+
+  db.ref().on(
+    "child_added",
+    function(snapshot) {
+      console.log(snapshot.val());
+      console.log(snapshot.val().name);
+      console.log(snapshot.val().destination);
+      console.log(snapshot.val().first);
+      console.log(snapshot.val().frequency);
+
+      var first = snapshot.val().first;
+      var frequency = snapshot.val().frequency;
+
+      var row = $("<tr></tr>");
+      var col1 = $("<td></td>").text(snapshot.val().name);
+      row.append(col1);
+
+      var col2 = $("<td></td>").text(snapshot.val().destination);
+      row.append(col2);
+
+      var col3 = $("<td></td>").text(snapshot.val().frequency);
+      row.append(col3);
+
+      var oneYearAgo = moment(first, "HH:mm").subtract(1, "years");
+
+      var timeDifference = moment().diff(moment(oneYearAgo), "minutes");
+
+      var remainder = timeDifference % frequency;
+
+      var minutesToArrive = frequency - remainder;
+
+      var next = moment().add(minutesToArrive, "minutes");
+
+      var col4 = $("<td></td>").text(next);
+      row.append(col4);
+
+      var col5 = $("<td></td>").text(minutesToArrive);
+      row.append(col5);
+
+      $("#train-schedule").append(row);
+    },
+    function(errorObject) {
+      console.log("Errors handled: " + errorObject.code);
+    }
+  );
+});
